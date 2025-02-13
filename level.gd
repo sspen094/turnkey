e6e591db13b23593d589fa58417c96a3c0c38bd3
Sam Spencer <sspen094@uottawa.ca>
2025-02-13 11:52:05 -0500

    Adding Level Changer

--- level.gd
+++ level.gd
@@ -2,6 +2,5 @@ extends Node2D
 
 func travelTo(sceneTo, location_x, location_y):
 	print(get_parent())
-	if "changeScene" in get_parent():
-		print("hello2")
-		get_parent().changeScene(sceneTo, location_x, location_y)
+	if "changeScene" in get_parent().get_parent():
+		get_parent().get_parent().changeScene(sceneTo, location_x, location_y)
--- ui.gd
+++ ui.gd
@@ -1,8 +1,17 @@
 extends Node2D
 
-func _ready() -> void:
-	get_tree().change_scene_to_file("res://level.tscn")
+var level = load("res://level.tscn")
+var levelInstance = level.instantiate()
 
-func changeScene(sceneTo, location_x, location_y):
-	get_tree().change_scene_to_file(sceneTo)
+func _ready() -> void:
+	get_node("LevelHolder").add_child(levelInstance)
 	
+func changeScene(sceneTo, location_x, location_y):
+	var newLevel = load(sceneTo)
+	var newLevelInstance = newLevel.instantiate()
+	if newLevelInstance.get_node("player") != null:
+		newLevelInstance.get_node("player").position.x = location_x
+		newLevelInstance.get_node("player").position.y = location_y
+	for child in get_node("LevelHolder").get_children():
+		child.queue_free()
+	get_node("LevelHolder").add_child(newLevelInstance)
